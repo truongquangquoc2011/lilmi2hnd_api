3b7428b72ee709e09c0c1bcc3e16fe147e628775
truongquangquoc2011 <truongquangquoc2011@gmail.com>
2026-05-12 14:23:35 +0700

    Implement onModuleDestroy method and enhance error handling in onModuleInit for PrismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,20 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      // Không dùng await trực tiếp ở đây để tránh làm treo quá trình khởi tạo app
+      this.$connect()
+        .then(() => console.log('✅ MongoDB Connected Successfully'))
+        .catch((err) => console.error('❌ MongoDB Connection Error:', err));
+    } catch (error) {
+      console.error('❌ Prisma Init Error:', error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
