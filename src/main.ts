9fa82fba57d76ad96ea6533c10801f2dd0799b32
truongquangquoc2011 <truongquangquoc2011@gmail.com>
2026-05-12 15:10:01 +0700

    Add ZodValidationPipe for request validation and update global prefix to '/api/'

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { envConfig } from './shared/config'
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ZodValidationPipe());
   app.setGlobalPrefix('/api/', {
     exclude: ['/docs', '/docs-json'],
   })
