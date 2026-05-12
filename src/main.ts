9ea226629c1af04cbf3214a5314c7dca41af1e27
truongquangquoc2011 <truongquangquoc2011@gmail.com>
2026-05-12 15:01:41 +0700

    Refactor bootstrap function to simplify initialization and remove error handling; streamline global prefix and CORS setup

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ZodValidationPipe } from 'nestjs-zod';
-import { envConfig } from './shared/config';
-
-let app;
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { envConfig } from './shared/config'
 
 async function bootstrap() {
-  try {
-    if (!app) {
-      console.log('🚀 [Bootstrap] Đang khởi tạo NestFactory...');
-      app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('/api/', {
+    exclude: ['/docs', '/docs-json'],
+  })
+  app.enableCors()
 
-      app.useGlobalPipes(new ZodValidationPipe());
-      app.setGlobalPrefix('api');
-      app.enableCors();
-
-      console.log('⏳ [Bootstrap] Đang gọi app.init()...');
-      await app.init();
-      console.log('✅ [Bootstrap] Khởi tạo thành công!');
-    }
-    return app.getHttpAdapter().getInstance();
-  } catch (error) {
-    console.error('❌ [Bootstrap] LỖI KHỞI CHẠY:', error.message);
-    console.error('❌ [Bootstrap] Chi tiết:', error.stack);
-    throw error;
-  }
+  await app.listen(envConfig.port ?? 3000)
 }
-
-if (process.env.NODE_ENV !== 'production') {
-  bootstrap();
-}
-
-export default bootstrap;
+bootstrap()
+  
